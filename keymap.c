be3556a9f58a5f2f8f8360b6f8785719f22110eb
tomtoru <tomtoru23@gmail.com>
2020-09-10 08:52:44 +0900

    upd

--- keymap.c
+++ keymap.c
@@ -46,11 +46,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QWERTY] = LAYOUT( \
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,      KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV, \
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,      KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
-  KC_LGUI,  KC_A,   KC_S,    KC_D,    KC_F,      KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,      KC_B,   KC_LBRC, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-                             KC_LALT, KC_LCTRL,  LOWER,  KC_SPC,  KC_ENT,  RAISE,   KC_BSPC, KC_RGUI \
+  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,      KC_5,                               KC_6,              KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV, \
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,      KC_T,                               KC_Y,              KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
+  KC_LGUI,  KC_A,   KC_S,    KC_D,    KC_F,      KC_G,                               KC_H,              KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,      KC_B,             KC_LBRC, KC_RBRC, KC_N,              KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+                             KC_LALT, KC_LCTRL,  LT(1, KC_LANG2),  KC_SPC,  KC_ENT,  LT(2, KC_LANG1),   KC_BSPC, KC_RGUI \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |   !  |   #  |   $  |   %  |   &  |-------.    ,-------|   4  |   5  |   6  |   +  |   `  |   @  |
  * |------+------+------+------+------+------|   (   |    |    )  |------+------+------+------+------+------|
- * |      |      |      |      |   :  |   .  |-------|    |-------|   1  |   2  |   3  |   =  |   *  |   _  |
+ * |      |      |      |   -  |   :  |   .  |-------|    |-------|   1  |   2  |   3  |   =  |   *  |   _  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt | LGUI |LOWER | /Space  /       \  0   \  |RAISE |BackSP| RGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,   KC_F12,\
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_7,    KC_8,    KC_9,    KC_TILD,  KC_CIRC,  KC_PIPE, \
   XXXXXXX, KC_EXLM, KC_HASH, KC_DLR,  KC_PERC, KC_AMPR,                   KC_4,    KC_5,    KC_6,    KC_PLUS,  KC_GRV,   KC_AT, \
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_COLN, KC_DOT,  KC_LPRN, KC_RPRN, KC_1,    KC_2,    KC_3,    KC_EQL,   KC_ASTR,  KC_UNDS, \
+  XXXXXXX, XXXXXXX, XXXXXXX, KC_MINS, KC_COLN, KC_DOT,  KC_LPRN, KC_RPRN, KC_1,    KC_2,    KC_3,    KC_EQL,   KC_ASTR,  KC_UNDS, \
                              _______, _______, _______, _______, KC_0,    _______, _______, _______\
 ),
 /* RAISE
