6d9bd108f1b44a031c2051efd4d33028c3def4df
tomtoru <tomtoru23@gmail.com>
2020-09-23 15:36:43 +0900

    upd

--- keymap.c
+++ keymap.c
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |   !  |   #  |   $  |   %  |   &  |-------.    ,-------|   4  |   5  |   6  |   +  |   `  |   @  |
  * |------+------+------+------+------+------|   (   |    |    )  |------+------+------+------+------+------|
- * |      |      |      |   -  |   :  |   .  |-------|    |-------|   1  |   2  |   3  |   =  |   *  |   _  |
+ * |      |      |   \  |   -  |   :  |   .  |-------|    |-------|   1  |   2  |   3  |   =  |   *  |   _  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt | LGUI |LOWER | /Space  /       \  0   \  |RAISE |BackSP| RGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
@@ -68,14 +68,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT( \
   KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,   KC_F12,\
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_7,    KC_8,    KC_9,    KC_TILD,  KC_CIRC,  KC_PIPE, \
-  XXXXXXX, KC_EXLM, KC_HASH, KC_DLR,  KC_PERC, KC_AMPR,                   KC_4,    KC_5,    KC_6,    KC_PLUS,  KC_GRV,   KC_AT, \
-  XXXXXXX, XXXXXXX, XXXXXXX, KC_MINS, KC_COLN, KC_DOT,  KC_LPRN, KC_RPRN, KC_1,    KC_2,    KC_3,    KC_EQL,   KC_ASTR,  KC_UNDS, \
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_7,    KC_8,    KC_9,    KC_TILD,  KC_CIRC,  KC_PIPE, \
+  _______, KC_EXLM, KC_HASH, KC_DLR,  KC_PERC, KC_AMPR,                   KC_4,    KC_5,    KC_6,    KC_PLUS,  KC_GRV,   KC_AT, \
+  _______, XXXXXXX, KC_BSLS, KC_MINS, KC_COLN, KC_DOT,  KC_LPRN, KC_RPRN, KC_1,    KC_2,    KC_3,    KC_EQL,   KC_ASTR,  KC_UNDS, \
                              _______, _______, _______, _______, KC_0,    _______, _______, _______\
 ),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |      |      |      |      |      |                    |      |      |      | MUTE | Vup  | Vdown|
+ * |      |      |      |      |      |      |                    |      |      |PLY/ST| MUTE | Vup  | Vdown|
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -89,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_RAISE] = LAYOUT( \
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX,  XXXXXXX, XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, \
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX,  XXXXXXX, KC_MPLY, KC_MUTE, KC_VOLD, KC_VOLU, \
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_LEFT,  KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX, \
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LANG2, KC_LABK,  KC_RABK,  KC_LANG1, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
